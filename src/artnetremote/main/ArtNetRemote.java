88535a44384abab0742ee95ea975c2108b8a0011
Jérémie GASTON-RAOUL <jeremie.gastonraoul@free.fr>
2010-02-19 02:33:12 +0100

    renamed main class

--- src/artnetremote/main/ArtNetRemote.java
+++ src/artnetremote/main/ArtNetRemote.java
@@ -2,6 +2,8 @@ package artnetremote.main;
 
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import artnetremote.gui.controllers.LogsController;
 import artnetremote.gui.controllers.PrefsController;
@@ -11,14 +13,14 @@ import artnetremote.gui.views.LogsView;
 import artnetremote.gui.views.PrefsView;
 import artnetremote.gui.views.RemoteView;
 
-public class Main {
+public class ArtNetRemote {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		RemoteModel remoteModel = new RemoteModel();
-		RemoteView remoteView = new RemoteView(remoteModel);
+		final RemoteView remoteView = new RemoteView(remoteModel);
 		RemoteController remoteController = new RemoteController(remoteModel,remoteView);
 		
 		PrefsView prefsView = new PrefsView(remoteModel);
@@ -28,7 +30,7 @@ public class Main {
 		LogsController logsController = new LogsController(remoteModel, logsView);
 		
 		JFrame frame = new JFrame("ArtNet Remote");
-		JTabbedPane tabbedPane = new JTabbedPane();
+		final JTabbedPane tabbedPane = new JTabbedPane();
 		frame.setContentPane(tabbedPane);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -36,10 +38,17 @@ public class Main {
 		tabbedPane.addTab("prefs",prefsView.getPrefsPanel());
 		tabbedPane.addTab("logs",logsView.getLogsPanel());
 		
+		tabbedPane.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				tabbedPane.getSelectedComponent().requestFocusInWindow();
+			}
+		});
+		tabbedPane.setSelectedIndex(0);
+		
 		frame.pack();
 		frame.setVisible(true);
+		
+		tabbedPane.getSelectedComponent().requestFocusInWindow();
 	}
-
-
-
 }
